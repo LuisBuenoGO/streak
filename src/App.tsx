3c3bd18d29e9ec66635bc196548b27bb33c71feb
Luis Bueno <luisgabrielbuenoorozco@gmail.com>
2026-08-22 13:05:07 -0700

    feat: create a project card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import ProjectCard from "./components/ProjectCard";
+
 function App() {
   return (
     <main>
       <h1>Streak</h1>
-      <p>Mis proyectos, mi progreso</p>
+      <p>Mis proyectos, mi progreso.</p>
+
+      <ProjectCard />
     </main>
   );
 }
---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,10 @@
+function ProjectCard() {
+  return (
+    <section>
+      <h2>SiloMusic</h2>
+      <p>Mi proyecto de música</p>
+    </section>
+  );
+}
+
+export default ProjectCard;
